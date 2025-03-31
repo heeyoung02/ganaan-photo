e2369b1781b5345a1ec857de9901d5984bc9dbe7
heeyoung <hhllldpei@gmail.com>
2025-03-31 15:41:24 +0900

    initial setting

---
+++ src/main/java/com/chuncheon/ganaanphoto/GanaanPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.chuncheon.ganaanphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanaanPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanaanPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chuncheon/ganaanphoto/GanaanPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuncheon.ganaanphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanaanPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
